fbe8bdfc72dc7eca881d99863d2d86c3467efd27
Min ho Kim <minho42@gmail.com>
2024-12-16 00:53:48 +1100

    add hash solution

--- 1.two_sum.py
+++ 1.two_sum.py
@@ -23,5 +23,20 @@ def two_sum(nums, target):
                 second = len(nums) - nums[::-1].index(a[j])- 1
                 return [first, second]
         
+
+def two_sum_hash(nums, target):
+    h = {}
+    
+    for i in range(len(nums)):
+        h[nums[i]] = i
+    
+    for i in range(len(nums)):
+        y = target - nums[i]
+        if y in h and h[y] != i:
+            return [i, h[y]]
+
 r = two_sum(nums, target)
 print(r)
+
+r2 = two_sum_hash(nums, target)
+print(r2)
