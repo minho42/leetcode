fee142c3f1af63db7c6c7124efb01c59b5e16e82
Min ho Kim <minho42@gmail.com>
2024-12-17 00:45:07 +1100

    14.longest_common_prefix.py

---
+++ 14.longest_common_prefix.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/longest-common-prefix/
+
+
+# Example 1:
+
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+# Example 2:
+
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+
+from typing import List
+
+
+def allStartsWith(strs, str):
+    r = set()
+    try:
+        for x in strs:
+            r.add(x.index(str))
+    except ValueError:
+        return False
+    return len(r) == 1
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        longest = ""
+        sorted_strs = sorted(strs, key=lambda x: len(x))
+        first = sorted_strs[0]
+
+        for i in range(len(first)):
+            text = first[0 : i + 1]
+            if allStartsWith(strs, text):
+                if len(text) > len(longest):
+                    longest = text
+        return longest
+
+
+s = Solution()
+print(s.longestCommonPrefix(["flower", "flow", "flight"]))
+print(s.longestCommonPrefix(["dog", "racecar", "car"]))
+print(s.longestCommonPrefix(["reflower", "flow", "flight"]))
