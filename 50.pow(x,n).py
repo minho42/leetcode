95222999dae9217b6c6ec59643c6b8ce3e0a2ce8
Min ho Kim <minho42@gmail.com>
2024-12-18 10:03:24 +1100

    50.pow(x,n)

---
+++ 50.pow(x,n).py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/powx-n/
+
+# Example 1:
+# Input: x = 2.00000, n = 10
+# Output: 1024.00000
+
+# Example 2:
+# Input: x = 2.10000, n = 3
+# Output: 9.26100
+
+# Example 3:
+# Input: x = 2.00000, n = -2
+# Output: 0.25000
+# Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+
+def myPow(x: float, n: int) -> float:
+    return x**n
+
+
+print(myPow(2.00000, 10))
+print(myPow(2.10000, 3))
+print(myPow(2.00000, -2))
