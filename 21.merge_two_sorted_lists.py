ae1cea3c1ac46ead31115713d3b7754fb9397171
Min ho Kim <minho42@gmail.com>
2024-12-27 18:07:22 +1100

    refactor: move some code to utils.py

--- 21.merge_two_sorted_lists.py
+++ 21.merge_two_sorted_lists.py
@@ -22,33 +22,6 @@ from typing import Optional
 # Recursion
 
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-    def __str__(self) -> str:
-        return f"{self.val}->{self.next}"
-
-    def __iter__(self):
-        cur = self
-        while cur:
-            yield cur.val
-            cur = cur.next
-
-    def to_list(self):
-        return list(self)
-
-
-def list_to_linked_list(li):
-    # print(list_to_linked_list([1,2,3])) => 1->2->3->None
-    next = None
-    for x in li[::-1]:
-        cur = ListNode(x, next)
-        next = cur
-    return cur
-
-
 # ref: https://youtu.be/XIdigk956u0?si=58wdNnBsgLkK3Vgn
 
 
