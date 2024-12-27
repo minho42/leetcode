42021aaf7bcd3e655cd7caa0de4d91f85d448261
Min ho Kim <minho42@gmail.com>
2024-12-27 21:19:18 +1100

    148.sort_list: actually sorting linked list without using builtin sort
    ref: leetcode
    https://youtu.be/TGveA1oFhrc?si=hJ-E5B9l19MWfkCM

--- 148.sort_list.py
+++ 148.sort_list.py
@@ -16,7 +16,8 @@ from utils import list_to_linked_list, ListNode
 
 
 class Solution:
-    def sortList(self, list1: Optional[ListNode]) -> Optional[ListNode]:
+    # using builtin sort
+    def sortList1(self, list1: Optional[ListNode]) -> Optional[ListNode]:
         head = ListNode()
         tail = head
 
@@ -35,7 +36,79 @@ class Solution:
 
         return head.next
 
+    # using merge sort myself
+    def sortList2(self, list1: Optional[ListNode]) -> Optional[ListNode]:
+        # base case
+        if not list1 or not list1.next:
+            return list1
+
+        n = 0
+        temp = list1
+        while temp:
+            temp = temp.next
+            n += 1
+
+        m = n // 2
+        left = list1
+        prev = None
+        for i in range(m):
+            prev = list1
+            list1 = list1.next
+        prev.next = None
+        right = list1
+
+        left = self.sortList2(left)
+        right = self.sortList2(right)
+        return self.merge(left, right)
+
+    def merge(self, l1: Optional[ListNode], l2: Optional[ListNode]):
+        head = ListNode()
+        tail = head
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+
+        if l1:
+            tail.next = l1
+        else:
+            tail.next = l2
+
+        return head.next
+
+    # ref: https://youtu.be/TGveA1oFhrc?si=hJ-E5B9l19MWfkCM
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # base case
+        if not head or not head.next:
+            return head
+
+        left = head
+        # neetcode used temp but temp is not requied here
+        mid = self.getMid(head)
+        right = mid.next
+        mid.next = None
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+        return self.merge(left, right)
+
+    # ref: https://youtu.be/TGveA1oFhrc?si=hJ-E5B9l19MWfkCM
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
 
 s = Solution()
+# print(s.sortList([]))
+# print(s.sortList(list_to_linked_list([2, 1])))
+# print(s.sortList(list_to_linked_list([3, 2, 1])))
 # print(s.sortList(list_to_linked_list([2, 2, 1, 3])))
 print(s.sortList(list_to_linked_list([4, 19, 14, 5, -3, 1, 8, 5, 11, 15])))
